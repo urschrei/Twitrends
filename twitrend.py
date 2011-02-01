fbd328c249d1d82f1a30a8dd474f01e43bab07c9
Stephan Hügel <urschrei@gmail.com>
2011-02-01 11:51:35 +0000

    Removing unused imports, and tidying spacing

--- twitrend.py
+++ twitrend.py
@@ -6,11 +6,10 @@ twitrend.py
 Created by Stephan Hügel on 2011-01-31
 """
 
+
 import sys
-import os
 import tweepy
-import json
-import StringIO
+
 
 def open_file(to_read):
     """ Open a text file for reading, and strip the carriage returns
@@ -18,6 +17,7 @@ def open_file(to_read):
     with open(to_read, 'r') as opened:
         # strip the newlines, and return the result as a list
         return [got_lines.rstrip() for got_lines in opened.readlines()]
+
 
 def main():
     """ main function
@@ -30,9 +30,9 @@ def main():
     # Ireland's WOEID:
     woeid = 23424803
     retrieved = api.trends_location(woeid)
-    chunk = retrieved[0]
     names = [trend["name"] for trend in retrieved[0]["trends"]]
     print names
+
 
 if __name__ == "__main__":
     try:
