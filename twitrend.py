fc0478836f7ce977f1b2afe1ffa880fafba71773
Stephan Hügel <urschrei@gmail.com>
2011-02-01 12:07:09 +0000

    Adding some error handling for OAuth credential and trend retrieval

--- twitrend.py
+++ twitrend.py
@@ -14,9 +14,13 @@ import tweepy
 def open_file(to_read):
     """ Open a text file for reading, and strip the carriage returns
     """
-    with open(to_read, 'r') as opened:
-        # strip the newlines, and return the result as a list
-        return [got_lines.rstrip() for got_lines in opened.readlines()]
+    try:
+        with open(to_read, 'r') as opened:
+            # strip the newlines, and return the result as a list
+            return [got_lines.rstrip() for got_lines in opened.readlines()]
+    except IOError:
+        print "Couldn't read OAuth values from %s. Can't continue." % to_read
+        raise
 
 
 def main():
@@ -25,11 +29,15 @@ def main():
     oauth_values = open_file('acc_keys.txt')
     auth = tweepy.OAuthHandler(oauth_values[0], oauth_values[1])
     auth.set_access_token(oauth_values[2], oauth_values[3])
-    api = tweepy.API(auth, secure = True)
-    # now we can do whatever we like!
-    # Ireland's WOEID:
-    woeid = 23424803
-    retrieved = api.trends_location(woeid)
+    try:
+        api = tweepy.API(auth, secure = True)
+        # now we can do whatever we like!
+        # Ireland's WOEID:
+        woeid = 23424803
+        retrieved = api.trends_location(woeid)
+    except tweepy.TweepError, err:
+        print "Couldn't retrieve trends. Error was: "
+        raise
     names = [trend["name"] for trend in retrieved[0]["trends"]]
     print names
 
